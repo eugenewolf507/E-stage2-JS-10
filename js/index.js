5a06303d19f1bf83f6d237b8110b592392a321fe
Yevhen Volkov <eugenewolf507@gmail.com>
2021-05-24 11:18:40 +0300

    feat: add fetch with then to get json file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goed here
+fetch('../../mock.json')
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(`Error: ${response.statusText}`);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });

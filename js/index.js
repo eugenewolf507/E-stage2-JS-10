2ddaeb6885338fb432e89f8d0187b86f461f8d83
Oleh Honchar <O.O.Honchar@gmail.com>
2021-05-24 11:29:22 +0300

    feat: add fetch to get employees from mock.json

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goed here
+'use strict';
+
+async function getEmployees() {
+    try {
+        const response = await fetch('./mock.json');
+        if (response.ok) {
+            let employees = await response.json();
+            console.log(employees);
+            return employees;
+        } else {
+            throw new Error(response.status);
+    }
+    } catch(error) {
+        console.error('Error: ', error);
+    }
+}
+
+getEmployees();
